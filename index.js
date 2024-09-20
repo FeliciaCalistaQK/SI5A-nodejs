0303627e0719a90531fbf6f11093cb28bfcc9fa9
FeliciaCalistaQK <feliciacalista18@gmail.com>
2024-09-20 11:11:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,19 @@ const server = http.createServer((req,res)=>{
     res.write('Selamat Pagi'); */
 
     res.writeHead(200, { 'Content-Type': 'application/json' });
+    if(req.url === '/dosen'){
+        message = 'List data dosen';
+        data = ['Ahmad','Hafiz','Widi'];
+    }else if(req.url === '/mahasiswa'){
+        message = 'List data Mahasisa';
+        data =['Pania','Khema','Fitri'];
+    }else{
+        message = 'Tidak ditemukan';
+        data=[];
+    }
     res.end(JSON.stringify({
-        'massage': 'Selamat Pagi',
+        'massage': message,
+        'status':'success',
         'status':'success'
     }));
 });
